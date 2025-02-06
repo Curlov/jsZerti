38ba3cc6d666359f03fa61dd7e7b6e0d2a6f6681
RaphaelStraschewski <r.Straschewski@edu.bbq.de>
2025-02-06 14:03:48 +0100

    index.js include enterButton

--- js/index.js
+++ js/index.js
@@ -1,17 +1,30 @@
-import { cards } from "./data/cards.js";
+import {cards} from "./data/cards.js";
 
 // How many cards are in the pool? Display it to the user.
-const totalNumberOfCards =  cards.length;
+const totalNumberOfCards = cards.length;
 document.querySelector('#totalNumberOfCards').innerHTML = totalNumberOfCards;
 
-// Eventlistener on button to select a set of cards
-document.querySelector('#submitCardRange').addEventListener('click', () => {
-  // Read start and end question
-  const start = document.querySelector('#startQuestion').value;
-  const end = document.querySelector('#endQuestion').value;
-  
-  // Redirect to card.html with start and end as GET params 
-  window.location.href = `card.html?start=${start}&end=${end}`;
-});
+// EventListener on button to select a set of cards
+document.querySelector('#submitCardRange').addEventListener('click', submitCardRange);
+
+// EventListener on keypress to select startQuestion/endQuestion
+document.querySelector('#startQuestion').addEventListener('keypress', handleKeyPress);
+document.querySelector('#endQuestion').addEventListener('keypress', handleKeyPress);
+
+// Function to include EventListener on keypress
+function handleKeyPress(event) {
+    if (event.key === 'Enter') {
+        submitCardRange();
+    }
+}
+
+// Function to read startQuestion/endQuestion
+function submitCardRange() {
+    const start = document.querySelector('#startQuestion').value;
+    const end = document.querySelector('#endQuestion').value;
+
+    // Redirect to card.html with start and end as GET params
+    window.location.href = `card.html?start=${start}&end=${end}`;
+}
 
 
